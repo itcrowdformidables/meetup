797a7305c6d55702d61ba5a61d6de54be7930de3
Ron Gebauer <ron.gebauer@raytion.com>
2020-03-08 20:57:17 +0100

    Start creation of project structure
    Signed-off-by: Ron Gebauer <ron.gebauer@raytion.com>

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    idea
+}
+
+group = "de.trainingHealth"
+version = "1.0.0"
+
+idea {
+    module {
+        isDownloadJavadoc = true
+        isDownloadSources = true
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "meetup"
+
+include("webapp")
